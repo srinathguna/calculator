0d2cf0d0454ed3603edf4658e2729b85c60f9160
SrinathG_Contus <srinath.g@contus.in>
2024-03-29 12:22:48 +0530

    new design added

--- js/calculator.js
+++ js/calculator.js
@@ -8,58 +8,88 @@ function createElement(tagname, attribute = {}) {
 }
 // Create container and input box
 const calcContainer = createElement('div', {
-     class: 'container col-xl-4'
+     class: 'container col col-xl-4 col-lg-4 col-md-8 col-sm-10 col-xs-12'
+})
+const rowDiv = createElement('div', {
+     class: 'row'
+})
+const rowNumDiv = createElement('div', {
+     class: 'row secondrow g-0'
+})
+const colDiv = createElement('div', {
+     class: 'col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'
+})
+const colNumDiv = createElement('div', {
+     class: 'num-col col col-xl-9 col-lg-9 col-md-9 col-sm-9 col-xs-9'
+})
+const colsplDiv = createElement('div', {
+     class: 'spl-col col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3'
 })
 const displayInputbox = createElement('input', {
-    class: 'col-xl-12 form-control display',
+    class: 'fs-2 col col-xl-12 col-lg-12 rounded-0 col-md-12 col-sm-12 col-xs-12 form-control display',
     id: 'result',
     placeholder: 0,
 })
 
 // Create buttons
 const cancelBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-info rounded-0 col col-xl-3 col-lg-3 col-md-3 col-sm-3',
     id: 'clear',
 })
 cancelBtn.textContent = "C"
 //
 const backspcBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-info rounded-0 col col-xl-3 col-lg-3 col-md-3 col-sm-3 bi bi-arrow-left',
 })
-backspcBtn.textContent = "<"
+backspcBtn.textContent = ""
 //
 const dotBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'fs-3 btn btn-l btn-outline-info rounded-0 col col-xl-3 col-lg-3 col-md-3 col-sm-3',
     id: 'dotval'
 })
 dotBtn.textContent = "."
 //
+const modulusBtn = createElement('button', {
+    type: 'button',
+    class: 'btn btn-lg btn-outline-warning rounded-0 col col-xl-12 col-lg-12 col-md-12 col-sm-12',
+    id: 'modulusval'
+})
+modulusBtn.textContent = "%"
+//
 const multipleBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-warning rounded-0 col col-xl-3 col-lg-3 col-md-3 col-sm-3',
     id: 'multipleval'
 })
 multipleBtn.textContent = "*"
 //
 const divideBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-warning rounded-0 col col-xl-12 col-lg-12 col-md-12 col-sm-12',
     id: 'divideval'
 })
 divideBtn.textContent = "/"
 //
 const minusBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-warning rounded-0 col col-xl-12 col-lg-12 col-md-12 col-sm-12',
     id: 'subtract'
 })
 minusBtn.textContent = "-"
 //
 const plusBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-warning rounded-0 col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12',
     id: 'add'
 })
 plusBtn.textContent = "+"
 //
 const equalBtn = createElement('button', {
-    class: 'btn btn-lg col-xl-3 border-0',
+    type: 'button',
+    class: 'btn btn-lg btn-outline-warning rounded-0 col col-xl-8 col-lg-8 col-md-8 col-sm-8 col-xs-8',
     id: 'equal'
 })
 equalBtn.textContent = "="
@@ -67,30 +97,41 @@ equalBtn.textContent = "="
 // Append elements to the document
 document.body.append(calcContainer);
 calcContainer.append(displayInputbox);
-calcContainer.append(cancelBtn);
-calcContainer.append(backspcBtn);
-calcContainer.append(dotBtn);
-calcContainer.append(multipleBtn);
-calcContainer.append(divideBtn);
-calcContainer.append(minusBtn);
-calcContainer.append(plusBtn);
-calcContainer.append(equalBtn);
+
+calcContainer.append(rowDiv);
+rowDiv.append(colDiv);
+
+calcContainer.append(rowNumDiv);
+rowNumDiv.append(colNumDiv);
+rowNumDiv.append(colsplDiv);
+
+colDiv.append(cancelBtn);
+colDiv.append(backspcBtn);
+colDiv.append(dotBtn);
+colDiv.append(multipleBtn);
+
+colsplDiv.append(divideBtn);
+colsplDiv.append(minusBtn);
+colsplDiv.append(plusBtn);
+colsplDiv.append(modulusBtn);
+
 
 // Create number buttons
-for (let i = 0; i <= 9; i++){
+for (let i = 9; i >= 0; i--){
     const createBtn = createElement('button', {
-        class: 'btn btn-lg col-xl-3 border-0',
+        type: 'button',
+        class: 'btn btn-lg btn-outline-secondary rounded-0 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-4',
         id: `${i}`
     })
     let value = createBtn.textContent = i;
-    calcContainer.append(createBtn);
+    colNumDiv.append(createBtn);
     let btnVal = document.getElementById(`${i}`)
     btnVal.addEventListener('click', () => {
         // console.log(value)
         displayInputbox.value += value
     })
 }
-
+colNumDiv.append(equalBtn);
 document.addEventListener('keypress', (event) => {
     const key = event.key;
     if (/\d/.test(key)) { // Check if the pressed key is a digit
@@ -135,9 +176,19 @@ dotVal.addEventListener('click', () => {
     displayInputbox.value += dotVal.textContent
 })
 
+let modulusVal = document.getElementById('modulusval')
+modulusVal.addEventListener('click', () => {
+    displayInputbox.value += modulusVal.textContent
+})
+
 let equalVal = document.getElementById('equal')
 equalVal.addEventListener('click', () => {
-    let result;
-    result = eval?.(displayInputbox.value)
-    displayInputbox.value = result
+    try {
+        let result;
+        result = eval?.(displayInputbox.value)
+        displayInputbox.value = result        
+    } catch (error) {
+        alert('Please give a valid expression') 
+        displayInputbox.value = ""        
+    }
 })   
